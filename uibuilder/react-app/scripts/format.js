3ef2fcac311c4380cda69043a4f970ccd9af40e3
Joe Zhou <joe.zhou@komaxgroup.com>
2022-01-28 11:33:26 +0800

    format index.js also

--- uibuilder/react-app/scripts/format.js
+++ uibuilder/react-app/scripts/format.js
@@ -1,7 +1,11 @@
 const fs = require('fs').promises;
 
-const targetFile = './dist/index.html';
+let targetFile = './dist/index.html';
+// format index.html
+main(targetFile)
 
+// format index.js
+targetFile = './dist/index.js';
 main(targetFile)
 
 async function main(file) {
@@ -9,6 +13,7 @@ async function main(file) {
   let result =  data.replace('/index.js', 'index.js');
   result =  result.replace('/style.css', 'style.css');
   result =  result.replace('/favicon.svg', 'favicon.svg');
+  result =  result.replace('/logo.svg', 'logo.svg');
   await fs.writeFile(file, result, 'utf8');
 }
 
